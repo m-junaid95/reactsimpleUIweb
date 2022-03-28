1988f45aa9cdf777d3ab75255caafb779ed4d827
Junaid <zulfiqar.junaid95@gmail.com>
2022-03-28 03:43:39 -0700

    Initial Commit

---
+++ src/Commom.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import web from "../src/images/img2.svg";
+import { NavLink } from "react-router-dom";
+import "./Commom.css";
+
+const Commom = (props) => {
+  return (
+    <>
+      <section id="header" className="d-flex align-items-center">
+        <div className="container-fluid ">
+          <div className="row">
+            <div className="col-10 mx-auto">
+              <div className="row">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                  <h1>
+                    {props.name}
+                    <strong className="brand-name" style={{ color: "#c79585" }}>
+                      {" "}
+                      MuhammadJunaid{" "}
+                    </strong>
+                  </h1>
+                  <h2 className="my-3">
+                    We are the team of talented developer making websites
+                  </h2>
+                  <div className="mt-3">
+                    <NavLink
+                      to={props.visit}
+                      className="get-started-btnn"
+                    >
+                      {props.btname}
+                    </NavLink>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 order-1 order-lg-2 header-img">
+                  <img
+                    src={props.imgsrc}
+                    className="img-fluid animated"
+                    style={{ border: "1px solid #c79585", borderRadius: "50%" }}
+                    alt="Commom img"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Commom;
---
+++ src/Sdata.jsx
@@ -0,0 +1,31 @@
+import android from "./images/android.jpeg";
+import digital from "./images/digital.jpeg";
+
+const Sdata = [
+  {
+    imgsrc: "https://img.freepik.com/free-photo/developing-programmer-development-website-design-coding-technologies_18497-1090.jpg?w=740",
+    title: "web development",
+  },
+  {
+    imgsrc: "https://img.freepik.com/free-photo/cropped-shot-startup-ux-ui-front-end-designers-developing-programming-coding-responsive-web-content-mobile-application-from-prototype-wireframe-layout_68339-164.jpg?w=740",
+    title: "App development",
+  },
+  {
+    imgsrc: "https://img.freepik.com/free-vector/web-development-isometric-concept-composition-illustration_1284-55922.jpg?t=st=1648397180~exp=1648397780~hmac=6d5ac4148063ec505fbe8ed7ee24c137bcfb2a6f493ab0e26171229d8924451b&w=740",
+    title: "Software development",
+  },
+  {
+    imgsrc: digital,
+    title: "Digital Marketing",
+  },
+  {
+    imgsrc: android,
+    title: "Android development",
+  },
+  {
+    imgsrc: "https://img.freepik.com/free-vector/digital-marketing-concept-with-online-advertising-media-symbols-flat_1284-31958.jpg?t=st=1648394395~exp=1648394995~hmac=43bf55ffbee4caf4b4ef2814602c3ccbd0958aa606faa56addc614377234163a&w=826",
+    title: "Marketing",
+  },
+];
+
+export default Sdata;
